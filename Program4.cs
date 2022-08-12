27c54a0684c277ed2131a84e266e7be7bdbb1604
theGreatKapybara <oliver.shustar@gmail.com>
2022-08-12 20:00:32 +0500

    4 hw made

---
+++ Program4.cs
@@ -0,0 +1,16 @@
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 2;
+if (a == 0)
+{
+    Console.WriteLine(a);
+}
+if (a == 1)
+{
+    Console.WriteLine(a);
+}
+while (b<=a)
+{
+    Console.WriteLine(b);
+    b=b+2;
+} 
